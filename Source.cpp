9271d2e74fb0d7b21719eb376e7e8d1afb45c124
mvbp <mvater.mvbp@live.com>
2016-03-30 23:46:25 -0400

    Initial main changes
    Update main class with all the includes and a head comment

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+/*
+Matthew Vater, Adam Mannheim, Nathan Spangler
+mav78@pitt.edu, amm349@pitt.edu, njs55@pitt.edu
+30 March 2016
+SPICE Simulator
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <armadillo>
+
 #include "\Python32\include\Python.h"
 
-int main()
-{
+#include "CurrentSource.h"
+#include "Element.h"
+#include "PowerSource.h"
+#include "Resistor.h"
+
+
+#define PYRUN(...) sprintf(cmd_str,__VA_ARGS__); (PyRun_SimpleString(cmd_str) < 0 ? getchar():1);
+
+
+int main(void) {
 
 }
+
